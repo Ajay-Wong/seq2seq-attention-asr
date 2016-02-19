64d13591ee10c1e834fd26187145ef75416ca09b
Justin Mao-Jones <justinmaojones@gmail.com>
2016-02-18 19:48:31 -0500

    added beam search and ability to switch to predicting the 39 phonemes instead of 61

--- timit/timit.lua
+++ timit/timit.lua
@@ -8,20 +8,25 @@ require 'hdf5';
 require 'xlua';
 require 'optim';
 require 'cunn';
---package.path = package.path .. ';/home/jmj418/torch_utils/?.lua'
---require 'torch_utils/sopt';
+require 'utils';
+require 'utils_timit';
 TrainUtils = require 'TrainUtils'
 
 opt                 = opt                or {}
 opt.device          = opt.device         or 1
 opt.datafile        = opt.datafile       or 'TIMIT/logmel.h5'
 opt.savedir         = opt.savedir        or 'TIMIT/output/logmel'
-opt.resumedir       = opt.resumedir      or opt.savedir
+opt.phonemes        = opt.phonemes       or 'TIMIT/phonemes.txt'
 opt.batchSize       = opt.batchSize      or 1
 opt.maxnorm         = opt.maxnorm        or 1
 opt.weightDecay     = opt.weightDecay    or 1e-4
 opt.maxnumsamples   = opt.maxnumsamples  or 1e20
 opt.colnormconstr   = opt.colnormconstr  or false -- column norm constraint
+opt.weightnoise     = opt.weightnoise    or 0
+opt.numPhonemes     = opt.numPhonemes    or 62
+opt.decode39        = opt.decode39       or true
+opt.predict39       = opt.predict39      or false
+opt.K               = opt.K              or 5 -- beam search width
 print(opt)
 
 cutorch.setDevice(opt.device)
@@ -30,8 +35,6 @@ cutorch.setDevice(opt.device)
 local file = hdf5.open(opt.datafile)
 data = file:all()
 file:close()
-
-numPhonemes = 62
 
 function processData(data)
     if type(data) == 'table' then
@@ -46,7 +49,11 @@ function processData(data)
         for k,f in pairs(data) do
             i=i+1
             dataset.x[i] = f.x:cuda()
-            dataset.y[i] = f.y:cuda()
+			if opt.predict39 then
+				dataset.y[i] = f.y39:cuda()
+			else
+				dataset.y[i] = f.y:cuda()
+			end
         end
         dataset.numSamples = i
         return dataset
@@ -59,6 +66,13 @@ end
 train = processData(data.train)
 valid = processData(data.valid)
 test  = processData(data.test)
+vocabs,maps = loadPhonemeVocabs(opt.phonemes)
+
+function mapPhonemes(sequence,map)
+    local new_sequence = sequence:clone()
+    new_sequence:apply(function(x) return map[x] end)
+    return new_sequence
+end
 
 ------------------ Model ------------------
 if model then
@@ -69,7 +83,7 @@ if model then
 	optimConfig = model.optimConfig
 	optimState  = model.optimState
 	gradnoise   = model.gradnoise
-	--model.outputDepth = model.outputDepth or numPhonemes
+	--model.outputDepth = model.outputDepth or opt.numPhonemes
 
 
 	model.opt = opt
@@ -110,9 +124,9 @@ else
 	model.hybridAttendFeatureMaps = 16
 	model.stateDepth              = 400
 	model.annotationDepth 		  = model.outputFrameSize*2
-	model.outputDepth             = numPhonemes
-	model.peepholes               = true
-	model.mlpDepth                = numPhonemes*2 
+	model.outputDepth             = opt.numPhonemes
+	model.peepholes               = false
+	model.mlpDepth                = opt.numPhonemes*2 
 
 	decoder_recurrent = nn.LSTM(model.stateDepth, model.stateDepth, model.peepholes)
 	dec_mlp_inp = nn.Identity()()
@@ -167,6 +181,9 @@ gradnoise   = gradnoise or {
 	gamma = 0.55,
 	t     = 0
 }
+if opt.weightnoise > 0 then
+	weightnoise = torch.randn(parameters:size()):cuda()
+end
 function Train()
 	local numSamples     = math.min(opt.maxnumsamples,train.numSamples)
 	local shuffle        = torch.randperm(numSamples):long()
@@ -188,6 +205,13 @@ function Train()
 
 			-- since data is variable length, we do each sample individually
 			for b=1,opt.batchSize do
+
+				-- weight noise
+				if opt.weightnoise > 0 then
+					weightnoise:randn(parameters:size()):mul(opt.weightnoise)
+					parameters:add(weightnoise)
+				end
+
 				-- grab data
 				local index     = shuffle[t+b-1]
 				local X         = train.x[index]
@@ -230,7 +254,8 @@ function Train()
 			
 			-- L2 regularization
 			if opt.weightDecay > 0 then
-				gradients:add(parameters*opt.weightDecay)
+				nll = nll + 0.5*opt.weightDecay*(parameters:norm()^2)
+				gradients:add(opt.weightDecay, parameters)
 			end
 
 			-- gradient noise
@@ -246,7 +271,7 @@ function Train()
 		if opt.colnormconstr then
 			TrainUtils.columnNormConstraintGraph(autoencoder)
 		end
-		if t % 100 == 0 then
+		if t % 500 == 0 then
 			print('\ntrain gradnorm =', gradnorms[#gradnorms])
 			local accuracy = numCorrect/numPredictions
 			print('train accuracy =',torch.round(100*100*accuracy)/100 .. '%')
@@ -263,9 +288,10 @@ function Evaluate(data)
 	local NLL            = 0
 	local numCorrect     = 0
 	local numPredictions = 0
+	local per            = torch.Tensor(numSamples)
 	autoencoder:evaluate()
-	for t=1,numSamples,opt.batchSize do
-		xlua.progress(t+opt.batchSize-1,numSamples)
+	for t=1,numSamples do
+		xlua.progress(t,numSamples)
 		local index      = t
 		local X          = data.x[index]
 		local Y          = data.y[index]
@@ -278,13 +304,38 @@ function Evaluate(data)
 		pred = pred:squeeze()
 		numCorrect       = numCorrect + torch.eq(pred,Y):sum()
 		numPredictions   = numPredictions + Y:nElement()
+
+		-- calculate PER
+		local annotation = encoder.output
+		local eos        = Y[T]
+		local K          = opt.K
+		local maxseqlen  = X:size(1)
+		local prediction = decoder:BeamSearch(annotation,eos,K,maxseqlen)
+		local dist
+		if opt.decode39 and (opt.predict39 == false) then
+			local pred39     = mapPhonemes(prediction,maps.map39)
+			local target39   = mapPhonemes(Y,maps.map39)
+			dist   	         = WagnerFischer(pred39,target39)
+		else
+			dist             = WagnerFischer(prediction,Y)
+		end
+		per[t]           = dist/T
 	end
 	local accuracy = numCorrect/numPredictions
 	NLL            = NLL/numSamples
-	return accuracy, NLL
+	local PER      = per:mean()
+	return accuracy, NLL, PER
 end
 
 ------------------ Run ------------------
+function updateList(val,list)
+	if not list then
+		return torch.Tensor(1):fill(val)
+	else
+		return torch.cat(list,torch.Tensor(1):fill(val),1)
+	end
+end
+
 function updateLog(log,accuracy,nll,gradnorms)
 	if log then
 		log.accuracy = torch.cat(log.accuracy,torch.Tensor(1):fill(accuracy),1)
@@ -321,6 +372,14 @@ local trainLog, validLog
 local bestAccuracy = 0
 for epoch = 1, numEpochs do
 	print('---------- epoch ' .. epoch .. '----------')
+	local start = sys.clock()
+	if optimConfigResets then
+		if optimConfigResets[epoch] then
+			for k,v in pairs(optimConfigResets[epoch]) do
+				optimConfig[k] = v
+			end
+		end
+	end
 	print('optimConfig = ')
 	print(optimConfig)
 	print('optimState  = ')
@@ -341,11 +400,13 @@ for epoch = 1, numEpochs do
 	local Vh_train    = decoder.Vh.output:float()
 	
 	local start = sys.clock()
-	local validAccuracy, validNLL = Evaluate(valid)
+	local validAccuracy, validNLL, validPER = Evaluate(valid)
 	print('\nvalidation time =', torch.round(10*(sys.clock()-start)/60)/10 .. ' minutes')
 	validLog = updateLog(validLog, validAccuracy, validNLL)
+	validLog.PER = updateList(validPER, validLog.PER)
 	print('valid Accuracy  =', torch.round(100*100*validAccuracy)/100 .. '%')
 	print('valid NLL       =', torch.round(100*validNLL)/100)
+	print('valid PER       =', torch.round(100*100*validPER)/100 .. '%')
 	local alpha_valid = decoder:alpha():float()
 	local Ws_valid    = decoder:Ws():float()
 	local Vh_valid    = decoder.Vh.output:float()
@@ -368,4 +429,6 @@ for epoch = 1, numEpochs do
 		bestAccuracy = validAccuracy
 		torch.save(paths.concat(opt.savedir,'model_best.t7'),model)
 	end
+	local time = math.floor((sys.clock() - start)/60)
+	print('\nepoch ' .. epoch .. ' completed in ' .. time .. ' minutes\n')
 end
