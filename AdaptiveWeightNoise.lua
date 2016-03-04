ffc945b6561964fa1fc7b94beb9878f57d3e89dc
Justin Mao-Jones <justinmaojones@gmail.com>
2016-03-04 17:38:32 -0500

    fixed adaptive weight noise

--- AdaptiveWeightNoise.lua
+++ AdaptiveWeightNoise.lua
@@ -1,41 +1,77 @@
 local AdaptiveWeightNoise, parent  = torch.class('nn.AdaptiveWeightNoise', 'nn.Module')
 
-function AdaptiveWeightNoise:__init(parameters,lambda)
+local eps = 1e-12
+
+function AdaptiveWeightNoise:__init(parameters,lambda,sigma_init)
 	parent.__init(self)
 	self.alpha_mu = 0
 	self.alpha_sigma2 = 1
 	self.n = parameters:size(1)
 	self.weight = torch.Tensor(2*self.n):type(parameters:type())
-	local mu,sigma2 = unpack(self.weight:split(self.n))
-	mu:copy(parameters)
-	sigma2:fill(1)
-	self.sigma = sigma2:clone()
-	self.sample = parameters:clone()
+	local sigma_init = sigma_init or 1
+	self:initialize(parameters,sigma_init)
+	self.sigma = parameters:clone():zero()
+	self.sigma2 = parameters:clone():zero()
+	self.sample = parameters:clone():zero()
 	self.gradWeight = self.weight:clone():zero()
-	self.gradOutput2 = parameters:clone():zero()
+	self.dLNds = parameters:clone():zero()
 	self.lambda = lambda or 1 -- equals 1/B, the multiplier on Lc
+end
+
+function AdaptiveWeightNoise:getWeights()
+	local n = self.n
+	local mu,sigma2 = unpack(self.weight:split(n))
+	return mu,sigma
 end
 
 function AdaptiveWeightNoise:Sample()
 	local n = self.n
-	local mu,sigma2 = unpack(self.weight:split(n))
+	local mu,s = unpack(self.weight:split(n))
+	local sigma2 = self.sigma2:copy(s):exp()
 	self.sigma:copy(sigma2):sqrt()
 	self.sample:randn(self.sample:size())
 	self.sample:cmul(self.sigma)
 	self.sample:add(mu)
+	--print('\nsample:mean()',self.sample:mean())
+	--print('sample:std()',self.sample:std())
 	return self.sample
 end
 
-function AdaptiveWeightNoise:updateOutput(nll)
+function AdaptiveWeightNoise:initialize(mu_init,sigma_init)
 	local n = self.n
-	local mu,sigma2 = unpack(self.weight:split(n))
+	-- s = log(sigma^2)
+	local mu,s = unpack(self.weight:split(n))
+	if type(mu) == 'number' then
+		mu:fill(mu_init)
+	else
+		mu:copy(mu_init)
+	end
+	if type(sigma_init) == 'number' then
+		--sigma2:fill(sigma_init^2)
+		s:fill(sigma_init^2):log()
+	else
+		--sigma2:copy(sigma_init):pow(2)
+		sigma2:copy(sigma_init):pow(2):log()
+	end
+end
+
+function AdaptiveWeightNoise:Mode()
+	local mu,_ = unpack(self.weight:split(self.n))
+	return mu
+end
+
+function AdaptiveWeightNoise:updateOutput(nll)
 	self.nll = nll
 	if self.lambda > 0 then
+		local n = self.n
+		-- s = log(sigma^2)
+		local mu,s = unpack(self.weight:split(n))
+		local sigma2 = self.sigma2:copy(s):exp()
 		self.alpha_mu = mu:mean()
-		self.alpha_sigma2 = sigma2:mean() + (mu-self.alpha_mu):pow(2):sum()/n
-		self.KL  = 0.5*(n*torch.log(self.alpha_sigma2) - torch.log(sigma2):sum())
+		self.alpha_sigma2 = math.max(eps,sigma2:mean() + (mu-self.alpha_mu):pow(2):sum()/n)
+		self.KL  = 0.5*(n*torch.log(self.alpha_sigma2) - s:sum())
 		self.KL  = self.KL + 0.5/self.alpha_sigma2*((mu-self.alpha_mu):pow(2):sum())
-		self.KL  = self.KL + 0.5/self.alpha_sigma2*(sigma2:sum()-n*self.alpha_sigma2)
+		self.KL  = self.KL + 0.5/self.alpha_sigma2*(sigma2:sum()) - n/2
 		self.L   = self.lambda*self.KL + self.nll
 	else
 		self.L = self.nll
@@ -45,19 +81,26 @@ end
 
 function AdaptiveWeightNoise:accGradParameters(input, gradOutput)
 	local n = self.n
-	local mu,sigma2 = unpack(self.weight:split(n))
-
-	self.gradOutput2:copy(gradOutput):pow(2)
-	
-	local gradmu,gradsigma2 = unpack(self.gradWeight:split(self.n))
+	-- s = log(sigma^2)
+	local mu,s = unpack(self.weight:split(n))
+	local sigma2 = self.sigma2:copy(s):exp()
+	self.dLNds:copy(gradOutput):pow(2):cmul(sigma2):mul(0.5)
+	local gradmu,grads = unpack(self.gradWeight:split(self.n))
 	if self.lambda > 0 then
 		self.alpha_mu = mu:mean()
-		self.alpha_sigma2 = sigma2:mean() + (mu-self.alpha_mu):pow(2):sum()/n
+		self.alpha_sigma2 = math.max(eps,sigma2:mean() + (mu-self.alpha_mu):pow(2):sum()/n)
 		gradmu:copy(mu):add(-self.alpha_mu):div(self.alpha_sigma2):mul(self.lambda)
-		gradsigma2:copy(sigma2):pow(-1):mul(-1):add(1/self.alpha_sigma2):mul(0.5*self.lambda)
+		grads:copy(sigma2):mul(self.lambda*0.5/self.alpha_sigma2):add(-self.lambda*0.5)
+		--print('\n||dLEdmu|| =',gradmu:norm())
+		--print('||dLEds|| =',grads:norm())
+		gradmu:add(gradOutput)
+		grads:add(self.dLNds);
+	else
+		gradmu:copy(gradOutput)
+		grads:copy(self.dLNds);
 	end
-	gradmu:add(gradOutput)
-	gradsigma2:add(self.gradOutput2);
+	--print('||dLNdmu|| =',gradOutput:norm())
+	--print('||dLNds|| =',self.dLNds:norm(),'\n')
 end
 	
 
